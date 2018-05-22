198a373c3d54d27f291c5d88e040abc357a6c8f7
Lucas Almeida MAC <lucasalmeida70@gmail.com>
2018-05-22 11:47:23 -0300

    Redux config + folders structure adjusments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Menu from '../Menu';
 import './index.scss';
 
 class Header extends Component {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import App from './App';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+   <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
